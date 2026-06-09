4a59ac512936274889988106855fed53259a33b5
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-09 21:16:52 +0200

    Update main.tsx
    Add cache

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,17 @@ import { AuthProvider } from './contexts/AuthContext'
 import { FavoritesProvider } from './contexts/FavoritesContext'
 import { SearchProvider } from './contexts/SearchContext'
 import { PortfolioProvider } from './contexts/PortfolioContext'
+// Importo il client di cache
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 
+// Creo l'istanza del client di cache
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <FavoritesProvider>
         <SearchProvider>
@@ -18,5 +24,6 @@ createRoot(document.getElementById('root')!).render(
         </SearchProvider>
       </FavoritesProvider>
     </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
