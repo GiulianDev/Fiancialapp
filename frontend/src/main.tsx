9c1dc4716347a9797eedf60c097b4dc66f02576f
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-09 20:26:54 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './contexts/AuthContext'
 import { FavoritesProvider } from './contexts/FavoritesContext'
+import { SearchProvider } from './contexts/SearchContext'
 import { PortfolioProvider } from './contexts/PortfolioContext'
 import './index.css'
 import App from './App.tsx'
@@ -10,9 +11,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <FavoritesProvider>
-        <PortfolioProvider>
-          <App />
-        </PortfolioProvider>
+        <SearchProvider>
+          <PortfolioProvider>
+            <App />
+          </PortfolioProvider>
+        </SearchProvider>
       </FavoritesProvider>
     </AuthProvider>
   </StrictMode>,
