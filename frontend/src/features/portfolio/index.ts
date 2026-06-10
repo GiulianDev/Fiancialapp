7d851e299290bebedff0ccc48a4b02d5c018f360
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-10 17:07:58 +0200

    Update index.ts

--- frontend/src/features/portfolio/index.ts
+++ frontend/src/features/portfolio/index.ts
@@ -1,11 +1,12 @@
 // gestire qui tutti i path per gli import dei componenti del portfolio, 
 // così da avere un punto unico di esportazione
-export type { SavedPortfolio } from '../../shared/services/portfolioService';
+export type { SavedPortfolio } from '@shared/services/portfolioService';
 export { PortfolioAnalysis } from './components/PortfolioAnalysis';
 export { FavoritesSelector } from './components/FavoritesSelector';
 
-export { Card } from '../../shared/ui/Card/Card';
-export { Button } from '../../shared/ui/Button';
+export { Card } from '@ui/Card/Card';
+export { Button } from '@ui/Button';
+// export { Button } from '@/shared/ui/Button';
 
-export { type AggregatedResult } from '../../shared/types/portfolio';
+export { type AggregatedResult } from '@/shared/types/portfolio';
 
