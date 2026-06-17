3022ba9aec6f1d1c2949b6135f9bd38b349e6f0f
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-17 09:06:45 +0200

    Update constants.ts

--- frontend/src/shared/config/constants.ts
+++ frontend/src/shared/config/constants.ts
@@ -3,3 +3,4 @@ export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://127.0.0.
 export const SEARCH_ETF_API_URL = API_BASE_URL + "/api/etf";
 export const HOLDING_DETAIL_API = API_BASE_URL + "/api/holding-details";
 export const HOLDING_HISTORY_API = API_BASE_URL + "/api/holding-history";
+export const HOLDING_FULL_API = API_BASE_URL + "/api/holding-full";
