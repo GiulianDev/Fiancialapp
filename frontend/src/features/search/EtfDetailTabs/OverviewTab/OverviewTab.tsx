7eb1394f2a23cbb3c6605c4260cd32ab3d16b1c2
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-22 18:56:30 +0200

    Create OverviewTab.tsx

---
+++ frontend/src/features/search/EtfDetailTabs/OverviewTab/OverviewTab.tsx
@@ -0,0 +1,13 @@
+
+interface OverviewTabProps {
+  isin: string;
+}
+
+export function OverviewTab({ isin }: OverviewTabProps) {
+
+  return (
+    <div className="space-y-6">
+     
+    </div>
+  );
+}
