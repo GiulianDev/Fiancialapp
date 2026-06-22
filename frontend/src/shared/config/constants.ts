3b600ce5aeae3171b48a0ace56861d086d1b2f67
GiulianDev <39489811+Giulio-91@users.noreply.github.com>
2026-06-22 20:22:55 +0200

    Update constants.ts

--- frontend/src/shared/config/constants.ts
+++ frontend/src/shared/config/constants.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8000";
 export const SEARCH_ETF_API_URL = API_BASE_URL + "/api/etf";
+export const ETF_INFO_API = API_BASE_URL + "/api/etf";
 
 /**
  * HOLDING API
